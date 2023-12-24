ac9ba3467b070730a5dbce7dc3b910a0d9868c07
Khalid Abd Elnaser <58325685+Khalid-MK@users.noreply.github.com>
2023-12-24 22:56:37 +0200

    Refactor Vite config to conditionally set plugins and base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Al-Roshd/',
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()]
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()],
+      base: '/Al-Roshd/',
+    }
+  }
 })
